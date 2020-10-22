9c957eab7412f2da3b9623bcba314c8fae44bf88
DoHuuLoc_D17-TH09_DH51703609 <locdo255@gmail.com>
2020-10-22 11:49:45 +0700

    Them ham tich

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b){
+	//Neu a hoac b null return 0
+	if(!a || !b) {
+		return 0;
+	}
+	else
+		// tra ve ket qua a nhan b
+		return (a*b);
+}
 float thuong(int a, int b);
 int main()
 {
