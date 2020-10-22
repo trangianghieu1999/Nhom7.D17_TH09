959c77b0d1e521742f1dbb55daab378e8f94a757
unknown <trangianghieu1999@gmail.com>
2020-10-22 11:45:34 +0700

    them ham tong

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,25 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
+
+//int hieu(int a, int b);
+//int tich(int a, int b);
+//float thuong(int a, int b);
 int main()
 {
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	int a,b;
+	cout <<"nhap a";
+	cin >> a;
+	cout << "nhap b";
+	cin>>b;
+	cout<<"ket qua tong a + b la "<<tong(a,b);
 	system("pause");
 	return 0;
+}
+int tong(int a, int b)
+{
+ 
+	return a+b;
 }
