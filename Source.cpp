f0219e327c1665c67e2eea6f2254854d40a2151c
unknown <dh51703213@student.stu.edu.vn>
2020-10-22 11:53:36 +0700

    them ham hieu

--- Source.cpp
+++ Source.cpp
@@ -7,8 +7,20 @@ int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a;
+	int b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout << "\nNhap a: ";
+	cin >> a;
+	cout << "\nNhap b: ";
+	cin >> b;
+	cout << "\nHieu cua a,b la: " << hieu(a,b);
 	system("pause");
 	return 0;
+}
+int hieu(int a, int b)
+{
+	int s = 0;
+	s = a - b;
+	return s;
 }
